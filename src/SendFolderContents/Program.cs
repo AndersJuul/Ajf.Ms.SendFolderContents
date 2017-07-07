4d0cbb9a7509c65956e967a7bf58161a1a585a2d
AndersJuul <andersjuulsfirma@gmail.com>
2017-07-07 14:25:39 +0200

    a

--- src/SendFolderContents/Program.cs
+++ src/SendFolderContents/Program.cs
@@ -9,67 +9,70 @@ namespace SendFolderContents
     {
         private static void Main(string[] args)
         {
-            StandardLoggerConfigurator.SetLogger();
+           Log.Logger= StandardLoggerConfigurator.GetLogger();
 
-            try
+            using (var context = StandardLoggerConfigurator.GetLogContext())
             {
-                var appSettings = new AppSettings();
-
-                HostFactory.Run(x => //1
+                try
                 {
-                    x.Service<Worker>(s => //2
+                    var appSettings = new AppSettings();
+
+                    HostFactory.Run(x => //1
                     {
-                        try
+                        x.Service<Worker>(s => //2
                         {
-                            s.ConstructUsing(name => new Worker(appSettings)); //3
-                            s.WhenStarted(tc =>
+                            try
                             {
-                                Log.Logger.Information("Starting service:" + appSettings.Revision);
-                                tc.Start();
-                                Log.Logger.Information("Service started.");
-                            }); //4
-                            s.WhenStopped(tc =>
-                            {
-                                Log.Logger.Information("Stopping service.");
-                                tc.Stop();
-                                Log.Logger.Information("Service stopped.");
-                            }); //5
-                            s.WhenPaused(tc =>
-                            {
-                                Log.Logger.Information("Pausing service.");
-                                tc.Stop();
-                                Log.Logger.Information("Service paused.");
-                            }); //5
+                                s.ConstructUsing(name => new Worker(appSettings)); //3
+                                s.WhenStarted(tc =>
+                                {
+                                    Log.Logger.Information("Starting service:" + appSettings.Revision);
+                                    tc.Start();
+                                    Log.Logger.Information("Service started.");
+                                }); //4
+                                s.WhenStopped(tc =>
+                                {
+                                    Log.Logger.Information("Stopping service.");
+                                    tc.Stop();
+                                    Log.Logger.Information("Service stopped.");
+                                }); //5
+                                s.WhenPaused(tc =>
+                                {
+                                    Log.Logger.Information("Pausing service.");
+                                    tc.Stop();
+                                    Log.Logger.Information("Service paused.");
+                                }); //5
 
-                            s.WhenContinued(tc =>
+                                s.WhenContinued(tc =>
+                                {
+                                    Log.Logger.Information("Continuing service.");
+                                    tc.Start();
+                                    Log.Logger.Information("Service continued.");
+                                }); //5
+                                s.WhenSessionChanged((w, sca) =>
+                                {
+                                    Log.Logger.Information("Session changed: " + w);
+                                    Log.Logger.Information("Session changed: " + sca);
+                                });
+                            }
+                            catch (Exception ex)
                             {
-                                Log.Logger.Information("Continuing service.");
-                                tc.Start();
-                                Log.Logger.Information("Service continued.");
-                            }); //5
-                            s.WhenSessionChanged((w, sca) =>
-                            {
-                                Log.Logger.Information("Session changed: " + w);
-                                Log.Logger.Information("Session changed: " + sca);
-                            });
-                        }
-                        catch (Exception ex)
-                        {
-                            Log.Error(ex, "Fra program");
-                            throw;
-                        }
-                    });
-                    x.RunAsLocalSystem(); //6
+                                Log.Error(ex, "Fra program");
+                                throw;
+                            }
+                        });
+                        x.RunAsLocalSystem(); //6
 
-                    x.SetDescription(appSettings.Description); //7
-                    x.SetDisplayName("AJF.SendFolderContents"); //8
-                    x.SetServiceName("AJF.SendFolderContents"); //9
-                }); //10        }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Yderste");
-                throw;
+                        x.SetDescription(appSettings.Description); //7
+                        x.SetDisplayName("AJF.SendFolderContents"); //8
+                        x.SetServiceName("AJF.SendFolderContents"); //9
+                    }); //10        }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, "Yderste");
+                    throw;
+                }
             }
         }
     }
